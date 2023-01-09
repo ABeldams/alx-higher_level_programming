5df6eb928e50fc5b6ea26f4c9e0dbb6388577d06
Aliyu Bello <aliyu.beldam1@gmail.com>
2023-01-09 23:15:52 +0000

    ddf

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
-<<<<<<< HEAD
 if __name__ == "__main__":    
     """Print the sum of 1 and 2."""
-=======
-if __name__ == "__main__":
->>>>>>> 8993898ec27fa93595b9f966633c0c4530b703d8
     from add_0 import add
 
     a = 1
